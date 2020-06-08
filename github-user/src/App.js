09226216bd904cbf5ded83ea6301891965707f86
stephanie mairena <stefyem7711@gmail.com>
2020-06-08 01:04:41 -0400

    setup

--- github-user/src/App.js
+++ github-user/src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
 
+/*
+## STEP 1 - Fetch the User Data
+
+- When your component mounts, send a GET request to the following URL (replacing the palceholder with your Github name):
+  - https://api.github.com/users/<your name>
+- After you fetch your data, set it to state
+*/
+
+
 function App() {
+//axios call
+axios.get('https://api.github.com/users/stefxm')
+  .then( response => {
+    console.log(response.data);   
+    })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p>
+         Hiiii JAY AND AVERIEEEE
+          MOMMY LOVES YOUUUUUU
+
+       </p>
       </header>
     </div>
   );
---
+++ github-user/src/Components/userCard.js
@@ -0,0 +1,18 @@
+/*## STEP 2 - Display the User Data
+
+- Pass the data to the component that will be displaying it
+- Build out a user card using the data that the Github API returns to you
+  - You may reference your old project for this, or you may wish to build this from scratch yourself
+  
+  ALSO
+  
+  ## STEP 4 - Style the User Card
+
+- Now it's time to style up your app
+- You are free to choose how you style your app, but make it look as presentable as you can
+- Try something new here. Maybe that's a new CSS technique you haven't really practiced yet. Maybe it's using a styling library you haven't tried. Push yourself to get better in this area.
+  */
+
+
+import React from "react";
+
